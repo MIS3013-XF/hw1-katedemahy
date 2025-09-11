f235cdf774fba9a5c567da5d8af6da9a2068ef78
de Mahy, Kate P. <Katherine.P.De.Mahy-1@ou.edu>
2025-09-11 17:13:56 -0500

    homework complete

--- HW1a_Sales/Program.cs
+++ HW1a_Sales/Program.cs
@@ -6,6 +6,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,8 +14,28 @@ namespace HW1a_Sales
 {
     internal class Program
     {
-        static void Main(string[hi] args)
+        private const double taxRate = 0.085; // const= never change double = to store numbers with decimals
+        static void Main(string[] args)
         {
+
+            Console.WriteLine("What is the product name of the item you are purchasing?"); // console.write displays without line break, readline waits for user to type something, add string and it stores it as a string, with itemName after string it saves the string as the name to use later
+            string itemName =Console.ReadLine();
+
+            Console.WriteLine($"How many {itemName}'s do you want to buy?"); // $ you can insert {itemName} right in the text
+            int quantity = Convert.ToInt32(Console.ReadLine()); // int.parse converts string to integer, readline waits for user to type something, we choose int cause quantity is always a whole #
+
+            Console.WriteLine($"What is the price of {itemName}?"); // double is for numbers with decimals
+            double price = Convert.ToDouble(Console.ReadLine());
+
+            double subtotal = quantity * price;
+            double salesTax = subtotal * taxRate;
+            double total = subtotal + salesTax;
+
+            Console.WriteLine(); // blank one to add line spacing between input and output really for aesthetics
+            Console.WriteLine($"Your subtotal for your bill is {subtotal:C}."); // C formats number as currency
+            Console.WriteLine($"The sales tax is {salesTax:C}.");
+            Console.WriteLine($"Your total for your bill is {total:C}.");
+
         }
     }
 }
--- HW1b_Grade/Program.cs
+++ HW1b_Grade/Program.cs
@@ -13,8 +13,44 @@ namespace HW1b_Grade
 {
     internal class Program
     {
+        private const double ASSIGNMENTS_WEIGHT = .20;
+        private const double EXAM1_WEIGHT = .15;
+        private const double EXAM2_WEIGHT = .25;
+        private const double EXAM3_WEIGHT = .25;
+        private const double PARTICIPATION_WEIGHT = .15;
+
         static void Main(string[] args)
         {
+            Console.WriteLine("What is your first name?");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("What is your last name?");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("What is your student ID number?");
+            string studentId = Console.ReadLine();
+
+            Console.WriteLine("What is your overall percentage grade for assignments?");
+            double assignmentGrade = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("What is your overall percentage grade for Exam 1?");
+            double exam1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("What is your overall percentage grade for Exam 2?");
+            double exam2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("What is your overall percentage grade for Exam 3?");
+            double exam3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("What is your overall percentage grade for participation?");
+            double participation = Convert.ToDouble(Console.ReadLine());
+
+            double finalGrade = (assignmentGrade * ASSIGNMENTS_WEIGHT) +
+                (exam1 * EXAM1_WEIGHT) + (exam2 * EXAM2_WEIGHT) +
+                (exam3 * EXAM3_WEIGHT) + (participation * PARTICIPATION_WEIGHT);
+
+            Console.WriteLine();
+            Console.WriteLine($"{firstName} {lastName}, {studentId}, Your final grade is {finalGrade}%.");
         }
     }
 }
